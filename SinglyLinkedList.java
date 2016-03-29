a628bbe8dbae2f5c7ceeb345d0542dc857ad506f
nhines04 <nhines04@gmail.com>
2016-03-29 17:40:52 -0400

    Added files via upload
    Added javadoc comments to LinkedListIterator.java, SingleLink.java, and SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,7 +1,25 @@
 import java.util.Iterator;
 
+/**
+* This class is responsible for generating the SinglyLinkedList<SingleLink> from
+* SingleLinks created by the SingleLink class
+*@author Team 3
+*@version "version 1.0 160329"
+*@since "version 1.0"
+*/
+
 public class SinglyLinkedList<T>{
+	/**
+	* Variable representing the head Link in the SinglyLinkedList
+	*/
 	Link<T> head;
+	
+	/**
+	* Overloaded constructor, builds the SinglyLinkedList from SingleLink
+	* objects based on the values in the array passed as a parameter
+	*@param array, the array containing the values which will be used to create
+	* the Links in the SinglyLinkedList
+	*/
 	public SinglyLinkedList(T[] array){
 		head = new Link<T>(array[array.length - 1]);
 		Link<T> temp;
@@ -12,6 +30,12 @@ public class SinglyLinkedList<T>{
 		}
 	}
 	
+	/**
+	* Method to generate an Iterator object for the SinglyLinkedList based
+	* on the current head Link
+	*@return LinkedListIterator object which contains a reference to the current
+	* and next Links
+	*/
 	public Iterator iterator(){
 		return new LinkedListIterator(head);
 	}
