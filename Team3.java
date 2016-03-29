9acefab64745d7b33673afb8eb97238748f59bfc
DavidPoduska <poduska.david@gmail.com>
2016-03-29 11:03:27 -0400

    Broke apart the linked list into separate files.

--- Team3.java
+++ Team3.java
@@ -1,7 +1,9 @@
+import java.util.Iterator;
+
 public class Team3{
-	SinglyLinkeList<Integer> list20, list2k, list20k;
+	SinglyLinkedList<Integer> list20, list2k, list20k;
 	
-	public Team3(int[] array1, int[] array2, int[] array3){
+	public Team3(Integer[] array1, Integer[] array2, Integer[] array3){
 		list20 = new SinglyLinkedList<Integer>(array1);
 		list2k = new SinglyLinkedList<Integer>(array2);
 		list20k = new SinglyLinkedList<Integer>(array3);
@@ -10,11 +12,11 @@ public class Team3{
 	public void testLists(){
 		long time;
 		time = testList(list20);
-		System.out.println("List of 20 integers took " + time + "ns to search.";
+		System.out.println("List of 20 integers took " + time + "ns to search.");
 		time = testList(list2k);
-		System.out.println("List of 2,000 integers took " + time + "ns to search.";
+		System.out.println("List of 2,000 integers took " + time + "ns to search.");
 		time = testList(list20k);
-		System.out.println("List of 20,000 integers took " + time + "ns to search.";
+		System.out.println("List of 20,000 integers took " + time + "ns to search.");
 	
 	}
 	public long testList20(){
@@ -27,12 +29,12 @@ public class Team3{
 		return testList(list20k);
 	}
 	
-	private long testList(SinglyLinkeList<Integer> list){
+	private long testList(SinglyLinkedList<Integer> list){
 		long startTime;
-		longFinishTime;
-		Iterator iterator = list.iterator(){
-		int data;
-		int intsFound;
+		long finishTime;
+		Iterator<Integer> iterator = list.iterator();
+		int data = 0;
+		int intsFound = 0;
 		//start the timer
 		startTime = System.nanoTime();
 		while(iterator.hasNext() && intsFound < 2){
