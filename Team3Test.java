ae157e960c322be482952c09196c20a4930d254a
mkrajcevx74 <mkrajcevx74@gmail.com>
2016-03-31 10:30:03 -0400

    Updated Team3Test.java

--- Team3Test.java
+++ Team3Test.java
@@ -1,45 +1,27 @@
-package edu.oakland.test;
-
-import edu.oakland.production.*;
-import edu.oakland.helper.*;
-import edu.oakland.helper02.*;
+package project.test;
+import project.production.*;
 import java.util.*;
-import junit.framework.*;
 
-/**
-* This class is designed to test the Team3.java, Link.java, 
-* SinglyLinkedList.java, and LinkedListIterator.java classes for Team 3's
-* assignment on the CSE 231 class project
-*@author Team 3
-*@version "version 1.0 160329"
-*@since "version 1.0"
-*/
-
-public class Team3Test extends TestCase{
-	private Integer[] test; 
-	private Team3 team3Test;
-	
-	public void setUp(){
-		test = new Integer[20];
-		evens = new int[2];
-		
-		for(int i = 0; i < 20; i++){
-			test[i] = 500 + i;
+public class Team3Test{
+	public static Team3 setUp(){
+		int i;
+		Integer[] arrRand1 = new Integer[20];
+		Integer[]arrRand2 = new Integer[2000];
+		Integer[] arrRand3 = new Integer[20000];
+		for(i=0;i<20;i++){
+			arrRand1[i] = (int)(Math.random()*199800)+20;
 		}
-		
-		System.out.print("Contents of test array, for visual inspection: ");
-		for(int i = 0; i < 19; i++){
-			System.out.print(test[i] + ", ");
+		for(i=0;i<2000;i++){
+			arrRand2[i] = (int)(Math.random()*199800)+20;
 		}
-		System.out.println(test[19]);
-		
-		team3Test = new Team3(test, test, test);
-		evens = team3Test.getFirstTwoEvens(team3Test.getList20()));
+		for(i=0;i<20000;i++){
+			arrRand3[i] = (int)(Math.random()*199800)+20;
+		}
+		return new Team3(arrRand1, arrRand2, arrRand3);
 	}
-	
-	public void testTeam3(){
-		assertEquals(500, evens[0]);
-		assertEquals(502, evens[1]);
-		assertEquals(15, evens[0]);
+	public static void main(String[] args){
+		
+		Team3 asdf = setUp();
+		asdf.testLists();
 	}
 }
